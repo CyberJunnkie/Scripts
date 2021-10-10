513344e9d252c24cb3e5af2bb86d28e20c0a25e5
CyberJunnkie <abdullahyasin3295@gmail.com>
2021-10-10 14:02:07 -0400

    Updated

--- build.sh
+++ build.sh
@@ -5,23 +5,23 @@ cd ~/tools
 
 wget https://github.com/sensepost/gowitness/releases/download/2.3.6/gowitness-2.3.6-linux-amd64
 mv gowitness-2.3.6-linux-amd64 gowitness
-mv gowitness /usr/bin
+mv gowitness /usr/local/bin
 
 
 wget https://github.com/tomnomnom/assetfinder/releases/download/v0.1.1/assetfinder-linux-amd64-0.1.1.tgz
 tar xvfz assetfinder-linux-amd64-0.1.1.tgz
-mv assetfinder /usr/bin
+mv assetfinder /usr/local/bin
 
 
 wget https://github.com/OWASP/Amass/releases/download/v3.13.4/amass_linux_amd64.zip
 unzip amass_linux_amd64.zip
 cd amass_linux_amd64
-mv amass /usr/bin
+mv amass /usr/local/bin
 
 
 wget https://github.com/tomnomnom/httprobe/releases/download/v0.1.2/httprobe-linux-amd64-0.1.2.tgz
 tar xvfz httprobe-linux-amd64-0.1.2.tgz
-mv httprobe /usr/bin
+mv httprobe /usr/local/bin
 
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 mv ngrok-stable-linux-amd64 ngrok.zip
